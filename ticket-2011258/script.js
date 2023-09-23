ea6e13c29d4ca24aab061042fb297930e176bed1
Raquelgloria <rakivaz16@.com>
2023-09-22 21:22:47 -0600

    ticket-ionic

--- ticket-2011258/script.js
+++ ticket-2011258/script.js
@@ -1,27 +1,26 @@
-        const itemsList = document.getElementById("items");
-        const ArticuloInput = document.getElementById("Articulo");
-        const ValorInput = document.getElementById("Valor");
-        const totalText = document.getElementById("total");
-        let total = 0;
+const item = document.getElementById("item"); 
+const ArticuloInput = document.getElementById("Articulo");
+const ValorInput = document.getElementById("Valor");
+const totalText = document.getElementById("total");
+let total = 0;
 
-        function calcularTotal() {
-            const name = ArticuloInput.value;
-            const price = parseFloat(ValorInput.value);
+function calcularTotal() {
+    const name = ArticuloInput.value;
+    const price = parseFloat(ValorInput.value);
 
-            if (name && !isNaN(price)) {
-                const item = document.createElement("li");
-                item.className = "item";
-                item.innerHTML = `<span>${name}</span><span>$${price.toFixed(2)}</span>`;
-                itemsList.appendChild(item);
+    if (name && !isNaN(price)) {
+        const itemElement = document.createElement("li");
+        itemElement.className = "item";
+        itemElement.innerHTML = `<span>${name}</span><span>$${price.toFixed(2)}</span>`;
+        item.appendChild(itemElement); 
 
-                total += price;
-                totalText.textContent = `Total: $${total.toFixed(2)}`;
+        total += price;
+        totalText.textContent = `Total: $${total.toFixed(2)}`;
 
-                ArticuloInput.value = "";
-                ValorInput.value = "";
-            }
-        }
-
+        ArticuloInput.value = "";
+        ValorInput.value = "";
+    }
+} 
 
     
 
