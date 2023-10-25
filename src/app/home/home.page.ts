605a49fd65ad5d9a373351324695265450433c4e
Raquelgloria <rakivaz16@gmail.com>
2023-10-25 12:41:43 -0600

    ejercicio 3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,7 +8,32 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {
-    console.log
-  }
+    console.log ("AppComponent: Constructor");
+   }
+   ngOnChanges() {
+    console.log("AppComponent: OnChanges");
+   }
+   ngOnInit() {
+    console.log("AppComponent: OnInit");
+   } 
+   ngDoCheck() {
+    console.log("AppComponent: DoCheck");
+   }
+   ngAfterContentInit() {
+    console.log("AppComponent: AfterContentInit");
+   }
+   ngAfterContentChecked() {
+    console.log("AppComponent:AfterContentChecked");
+   }
+   ngAfterViewInit() {
+    console.log("AppComponent:AfterViewInit");
+   }
+   ngAfterViewChecked() {
+    console.log("AppComponent:AfterViewChecked");
+   }
+   ngOnDestroy() {
+    console.log("AppComponent:OnDestroy ");
+   }
+
 
 }
